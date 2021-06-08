fb69420e817d9a22908a027b07f5f6f4128b4a73
Alex Potapov <alexanderpv@inbox.ru>
2021-06-08 14:05:22 +0300

    Added new functions in get_products.py and fixed error

--- get_products.py
+++ get_products.py
@@ -1,26 +1,97 @@
+from os import name
 import requests
 import logging
 from bs4 import BeautifulSoup
+from time import sleep
+from random import randrange
 
 
 logging.basicConfig(filename='parser.log', level=logging.INFO)
 
-def get_html():
-    try:
-        headers = {
-            'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:89.0) Gecko/20100101 Firefox/89.0"
-        }
+headers = {
+    "Accept":  "*/*",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36"
+}
 
-        pages = []
-        for i in range(1, 5):
-            url = f"https://www.avito.ru/moskva/tovary_dlya_kompyutera/komplektuyuschie/protsessory-ASgBAgICAkTGB~pm7gniZw?cd=1&p={i}"
-            response = requests.get(url, headers=headers)
-            response.raise_for_status()
-            pages.append(response.text)
-            return pages
+def get_html(page=1):
+    '''Функция get_html возвращает первую страницу, если не возникает сетевых ошибок'''
+    try:
+        url = f"https://www.avito.ru/moskva/planshety_i_elektronnye_knigi/elektronnye_knigi-ASgBAgICAUSYAohO?cd=1&p={page}"
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        return response.text
     except(requests.RequestException, ValueError):
         logging.info(f'Ошибка {response.status_code}')
         return False
     except Exception as er:
         logging.info(er)
         return False
+
+def get_products_links():
+    '''Функция get_products_links возвращает список ссылок на все товары категории'''
+    main_page = get_html()
+    if main_page:
+        soup = BeautifulSoup(main_page, 'lxml')
+        # Определяет количество страниц товаров данной категории
+        count_pages = int(soup.find('div', class_="pagination-root-2oCjZ").find_all('span', class_="pagination-item-1WyVp")[-2].text)
+        
+        links = []
+        for i in range(1, 2): # test 
+            page = get_html(i)
+            soup = BeautifulSoup(page, 'lxml')
+            item_card = soup.find_all('div', class_="iva-item-content-m2FiN")
+            for item in item_card:
+                link = f"https://www.avito.ru{item.find('div', class_='iva-item-titleStep-2bjuh').find('a').get('href')}"
+                links.append(link)
+                # sleep(randrange(5, 7))
+        return links
+    return False
+
+def get_product_html():
+    '''Функция get_product_html возвращает список html-страниц по каждому товару категории'''
+    links = get_products_links()
+
+    html_of_products = []
+    for link in links:
+        try:
+            response = requests.get(link, headers=headers)
+            response.raise_for_status()
+        except(requests.RequestException, ValueError):
+            logging.info(f'Ошибка {response.status_code}')
+            continue
+        except Exception as er:
+            logging.info(er)
+            continue
+        html_of_products.append(response.text)
+        sleep(randrange(5, 7))
+    return html_of_products
+
+def get_product_info():
+    '''Функция get_product_info выводит информацию по каждому товару'''
+    html_of_products = get_product_html()
+
+    for html in html_of_products:
+        soup = BeautifulSoup(html, 'lxml')
+
+        name = soup.find('div', class_="item-view-content").find('span', class_="title-info-title-text").text.strip()
+        id = soup.find('div', class_="item-view-content-right").find('div', class_="item-view-search-info-redesign").find('span').text.strip()[2:]
+        published = soup.find('div', class_="title-info-actions").find('div', class_="title-info-metadata-item-redesign").text.strip()
+        link_photo = soup.find('div', class_="item-view-content").find('div', class_="gallery-img-frame js-gallery-img-frame").get('data-url').strip()
+        addres = soup.find('div', class_="item-view-block item-view-map js-item-view-map").find('span', class_="item-address__string").text.strip()
+        price = soup.find('div', class_="item-view-content-right").find('span', class_="js-item-price").text.strip()
+        try:
+            description = soup.find('div', class_="item-description").text.strip()
+        except AttributeError:
+            description = ''
+
+        print(name)
+        print(id)
+        print(published)
+        print(link_photo)
+        print(addres)
+        print(price)
+        print(description)
+        print('----------')
+     
+if __name__ == "__main__":
+    get_product_info()
