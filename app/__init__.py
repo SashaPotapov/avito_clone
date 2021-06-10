157f56412f7ee5adb6a24f5f1b5a850fba28d787
Alex Potapov <alexanderpv@inbox.ru>
2021-06-10 22:23:43 +0300

    Added first teplates, create view function 'index'

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
 
-from app.models import db
+from app.models import db, Product
 
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
     db.init_app(app)
+    bootstrap = Bootstrap(app) 
 
     @app.route('/')
+    @app.route('/index')
     def index():
-        return 'Hello!'
+        products = Product.query.order_by(Product.published.desc()).all()
+        return render_template('index.html', title='Авито', products=products)
 
     return app
