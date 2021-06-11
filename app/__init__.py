5a849748da1a4bf970085eef4aebb6f62c39cf1b
Alex Potapov <alexanderpv@inbox.ru>
2021-06-11 17:27:52 +0300

    Added pagination

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 
@@ -13,7 +13,13 @@ def create_app():
     @app.route('/')
     @app.route('/index')
     def index():
-        products = Product.query.order_by(Product.published.desc()).all()
-        return render_template('index.html', title='Авито', products=products)
+        page = request.args.get('page', 1, type=int)
+        pagination = Product.query.order_by(Product.published.desc()).paginate(page, error_out=False)
+        products = pagination.items
+        next_url = url_for('index', page=pagination.next_num)
+        page = url_for('index', page=pagination.page)
+        prev_url = url_for('index', page=pagination.prev_num)
+        return render_template('index.html', title='Авитоклон', products=products, 
+                                pagination=pagination, next_url=next_url, prev_url=prev_url, page=page)
 
     return app
