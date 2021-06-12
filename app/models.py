8f93af359754df4ee186612db39872fe965df1f9
root <root@DESKTOP-KOB6V5M.localdomain>
2021-06-12 16:22:47 +0300

    Add db models for users/roles

--- app/models.py
+++ app/models.py
@@ -3,17 +3,40 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role')
+
+    def __repr__(self):
+        return f'<Role {self.name} {self.id}>'
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    email = db.Column(db.String(64), unique=True)
+    date_birth = db.Column(db.Date)
+    adress = db.Column(db.Text)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    products = db.relationship('Product', backref='user')
+
+    def __repr__(self):
+        return f'<User {self.username} {self.id}>'
+
 class Product(db.Model):
     __tablename__ = 'products'
     id = db.Column(db.Integer, primary_key=True)
     avito_id = db.Column(db.String, unique=True)
-    name = db.Column(db.String, nullable=False)
+    title = db.Column(db.String, nullable=False)
     published = db.Column(db.DateTime, nullable=False)
     link_photo = db.Column(db.String, nullable=False)
     price = db.Column(db.String, nullable=False)
     description = db.Column(db.Text, nullable=True)
     address = db.Column(db.Text, nullable=True)
     category = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
     def __repr__(self):
-        return f'<Item {self.name} {self.id}>'
+        return f'<Product {self.title} {self.id}>'
