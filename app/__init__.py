126f5bebc4cde4d871679df915022c9a86c6c1b6
MorganFrenk <morganfrenk2@gmail.com>
2021-06-15 22:58:41 +0300

    Add url_pref for auth

--- app/__init__.py
+++ app/__init__.py
@@ -20,6 +20,6 @@ def create_app(config_name):
     app.register_blueprint(main_blueprint)
 
     from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint)
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
     return app
