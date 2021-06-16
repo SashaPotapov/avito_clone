e616c27e0f5c98d72f70628e6149fc1a0c4b690c
MorganFrenk <morganfrenk2@gmail.com>
2021-06-16 23:49:08 +0300

    Add mail service

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
 from flask_login import LoginManager
+from flask_mail import Mail
 from config import config
 
 bootstrap = Bootstrap()
@@ -10,6 +11,7 @@ mail = Mail()
 db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
+mail = Mail()
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -19,6 +21,7 @@ def create_app(config_name):
     mail.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
+    mail.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
