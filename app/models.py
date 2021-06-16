ae5098f99393b333ddcff24fb6b9aded73050a9c
MorganFrenk <morganfrenk2@gmail.com>
2021-06-16 20:35:30 +0300

    Update User db table with serialisation methods for email confirmation

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flask import current_app
 from . import db, login_manager
 
 class Role(db.Model):
@@ -20,7 +22,8 @@ class User(db.Model, UserMixin):
     password_hash = db.Column(db.String(128))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     products = db.relationship('Product', backref='user')
-    
+    confirmed = db.Column(db.Boolean, default=False)
+
     @property
     def password(self):
         raise AttributeError('password is not a readable attribute')
@@ -31,6 +34,20 @@ class User(db.Model, UserMixin):
     
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def generate_confirmation_token(self, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'confirm': self.id}).decode('utf-8')
+
+    def confirm(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token.encode('utf-8'))
+        except:
+            return False
+        self.confirmed = True
+        db.session.add(self)
+        return True
 
     def __repr__(self):
         return f'<User {self.username} {self.id}>'
