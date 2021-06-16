79b52696ee9215476a3dc123f803deacff617f8d
Alexander <alexanderpv@inbox.ru>
2021-06-16 13:11:17 +0300

    Fixed pagination; Added view and template for each product; Fixed get_products.py; Changed Bootstrap version in templates/base.html

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,9 @@
+from flask import abort
 from flask import Flask, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 
-from app.models import db, Product
+from app.models import User, db, Product
 
 def create_app():
     app = Flask(__name__)
@@ -17,10 +18,17 @@ def create_app():
         pagination = Product.query.order_by(Product.published.desc()).paginate(page, error_out=False)
         products = pagination.items
         next_url = url_for('index', page=pagination.next_num)
-        page = url_for('index', page=pagination.page)
         prev_url = url_for('index', page=pagination.prev_num)
         return render_template('index.html', title='Авитоклон', products=products, 
-                                pagination=pagination, next_url=next_url, prev_url=prev_url, page=page)
+            pagination=pagination, next_url=next_url, prev_url=prev_url)
+
+    @app.route('/product/<int:product_id>')
+    def product_page(product_id):
+        product = Product.query.filter(Product.id == product_id).first()
+
+        if not product:
+            abort(404)
+        return render_template('product.html', product=product)
 
     @app.errorhandler(404)
     def page_not_found(e):
@@ -29,7 +37,5 @@ def create_app():
     @app.errorhandler(500)
     def internal_server_error(e):
         return render_template('500.html'), 500
-
-
 
     return app
