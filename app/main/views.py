7507e1a88632efa4f9d700eeb876d4336813d4c1
MorganFrenk <morganfrenk2@gmail.com>
2021-06-18 10:11:50 +0300

    Add Sasha's code to this branch (fix conflict)

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,4 @@
-from flask import request, render_template, url_for
+from flask import request, render_template, url_for, abort
 from . import main
 from .. import db
 from ..models import Product
@@ -9,8 +9,14 @@ def index():
     page = request.args.get('page', 1, type=int)
     pagination = Product.query.order_by(Product.published.desc()).paginate(page, error_out=False)
     products = pagination.items
-    next_url = url_for('.index', page=pagination.next_num)
-    page = url_for('.index', page=pagination.page)
-    prev_url = url_for('.index', page=pagination.prev_num)
+    next_url = url_for('main.index', page=pagination.next_num)
+    prev_url = url_for('main.index', page=pagination.prev_num)
     return render_template('main/index.html', title='Авитоклон', products=products, 
-                            pagination=pagination, next_url=next_url, prev_url=prev_url, page=page)
+                           pagination=pagination, next_url=next_url, prev_url=prev_url)
+
+@main.route('/product/<int:product_id>')
+def product_page(product_id):
+    product = Product.query.filter(Product.id == product_id).first()
+    if not product:
+        abort(404)
+    return render_template('main/product.html', product=product)
--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -1,4 +1,6 @@
+from time import sleep
 import unittest
+from app import db
 from app.models import User
 
 class UserModelTestCase(unittest.TestCase):
@@ -21,4 +23,26 @@ class UserModelTestCase(unittest.TestCase):
         u2 = User(password='cat')
         self.assertTrue(u.password_hash != u2.password_hash)
 
-    # add test for token
+    def test_token_confirmation(self):
+        u = User(name='cat', password = 'cat', address='cat')
+        db.session.add(u)
+        db.session.commit()
+        token = u.generate_confirmation_token()
+        self.assertTrue(u.confirm(token))
+
+    def test_token_expiration(self):
+        u = User(name='cat', password = 'cat', address='cat')
+        db.session.add(u)
+        db.session.commit()
+        token = u.generate_confirmation_token(1)
+        sleep(2)
+        self.assertFalse(u.confirm(token))
+
+    def test_token_unique(self):
+        u = User(name='cat', password = 'cat', address='cat')
+        u2 = User(name='cat', password = 'cat', address='cat')
+        db.session.add(u)
+        db.session.add(u2)
+        db.session.commit()
+        token = u.generate_confirmation_token()
+        self.assertFalse(u2.confirm(token))
