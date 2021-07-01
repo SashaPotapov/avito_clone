100543969ab36d6889dfe5b3e9a1ffe7704db456
MorganFrenk <morganfrenk2@gmail.com>
2021-07-01 23:03:17 +0300

    Add photo to new product

--- app/profile/forms.py
+++ app/profile/forms.py
@@ -7,7 +7,7 @@ from ..models import User
 
 class AddProdForm(FlaskForm):
     title = StringField('Название товара', validators=[DataRequired(), Length(1, 64)], render_kw={'class': 'form-control'})
-    price = DecimalField('Цена', validators=[DataRequired(), Length(1, 64)], render_kw={'class': 'form-control'})
+    price = DecimalField('Цена', validators=[DataRequired()], render_kw={'class': 'form-control'})
     description = TextAreaField('Описание (опционально)', validators=[Optional()], render_kw={'class': 'form-control'})
     address = StringField('Адрес (опционально)', validators=[Optional(), Length(1, 64)], render_kw={'class': 'form-control'})
     link_photo = FileField('Загрузите изображение товара', validators=[FileAllowed(['jpg', 'png'])], render_kw={'class': 'form-control-file'})
--- app/profile/views.py
+++ app/profile/views.py
@@ -1,5 +1,7 @@
+import os
+import secrets 
 from datetime import datetime
-from flask import render_template, redirect, url_for, abort
+from flask import render_template, redirect, url_for, abort, current_app
 from flask_login import login_required, current_user
 from flask_user import roles_required
 from . import profile
@@ -17,17 +19,29 @@ def user_profile(user_id):
         abort(404)
     return render_template('profile/user.html', user=user)
 
+def save_photo(form_photo):
+    random_hex = secrets.token_hex(8)
+    _, photo_ext = os.path.splitext(form_photo.filename)
+    photo_n = random_hex + photo_ext
+    photo_path = os.path.join(current_app.root_path, 'static/product_image/', photo_n)
+    form_photo.save(photo_path)
+    return photo_n
+    
 @profile.route('/profile/create_product', methods=['GET', 'POST'])
 @login_required
 def create_product():   
     form = AddProdForm()
     if form.validate_on_submit():
         prod = Product(title=form.title.data, published=datetime.today().strftime('%d.%m.%Y %H:%M'),
-                       price=form.price.data, description=form.description.data, address=form.address.data)
-                       #category=form.category.data, link_photo=form.link_photo.data)
+                       price=form.price.data, description=form.description.data, address=form.address.data,
+                       category='Электронные книги', user_id=current_user.id)
         db.session.add(prod)
         prod.avito_id = prod.id
+        if form.link_photo.data:
+            prod.link_photo = save_photo(form.link_photo.data)
+        
         db.session.commit()
+        return redirect(url_for('profile.user_profile', user_id=current_user.id))
     return render_template('profile/create_product.html', form=form)
 
 @profile.route('/profile/<int:user_id>/user_products')
