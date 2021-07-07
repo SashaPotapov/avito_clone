44933ce1c13c37494ef7dcaf22727eb4e4515297
MorganFrenk <morganfrenk2@gmail.com>
2021-07-07 19:16:54 +0300

    Delete redundant forms

--- app/profile/forms.py
+++ app/profile/forms.py
@@ -6,24 +6,6 @@ from wtforms.validators import DataRequired, Length, Regexp, Optional, Email, Eq
 from wtforms import ValidationError
 from ..models import User
 
-class AddProdForm(FlaskForm):
-    title = StringField('Название товара', validators=[DataRequired(), Length(1, 64)], render_kw={'class': 'form-control'})
-    price = DecimalField('Цена', validators=[DataRequired()], render_kw={'class': 'form-control'})
-    description = TextAreaField('Описание (опционально)', validators=[Optional()], render_kw={'class': 'form-control'})
-    address = StringField('Адрес (опционально)', validators=[Optional(), Length(1, 64)], render_kw={'class': 'form-control'})
-    link_photo = FileField('Загрузите изображение товара', validators=[FileAllowed(['jpg', 'png'])], render_kw={'class': 'form-control-file'})
-    # category
-    submit = SubmitField('Добавить товар', render_kw={'class': 'btn btn-primary'})
-
-
-class EditProdForm(FlaskForm):
-    title = StringField('Название товара', validators=[DataRequired(), Length(1, 64)], render_kw={'class': 'form-control'})
-    price = DecimalField('Цена', validators=[DataRequired()], render_kw={'class': 'form-control'})
-    description = TextAreaField('Описание (опционально)', validators=[Optional()], render_kw={'class': 'form-control'})
-    address = StringField('Адрес (опционально)', validators=[Optional(), Length(1, 64)], render_kw={'class': 'form-control'})
-    link_photo = FileField('Загрузите новое изображение товара', validators=[FileAllowed(['jpg', 'png'])], render_kw={'class': 'form-control-file'})
-    submit = SubmitField('Изменить', render_kw={'class': 'btn btn-primary'})
-
 
 class ChangePassForm(FlaskForm):
     password_old = PasswordField('Введите старый пароль', validators=[DataRequired()], render_kw={'class': 'form-control'})
