b372f993a05b004e9712c96159a7773344e0ee34
MorganFrenk <morganfrenk2@gmail.com>
2021-07-14 11:21:17 +0300

    Remove form from g

--- app/main/views.py
+++ app/main/views.py
@@ -5,11 +5,6 @@ from .. import db
 from ..models import Product, User
 
 
-@main.before_app_request
-def before_request():
-    g.search_form = SearchForm()
-
-
 @main.route('/')
 @main.route('/index')
 def index():
@@ -32,16 +27,17 @@ def product_page(product_id):
 
 @main.route('/search')
 def search():
-    if not g.search_form.validate():
+    form = SearchForm()
+    if not form.validate():
         return redirect(url_for('main.index'))
     page = request.args.get('page', 1, type=int)
-    products, total = Product.search(g.search_form.q.data, page, 20)
-    next_url = url_for('main.search', q=g.search_form.q.data, page=page+1) \
+    products, total = Product.search(form.q.data, page, 20)
+    next_url = url_for('main.search', q=form.q.data, page=page+1) \
         if total > page * 20 else None
-    prev_url = url_for('main.search', q=g.search_form.q.data, page=page-1) \
+    prev_url = url_for('main.search', q=form.q.data, page=page-1) \
         if page > 1 else None
     pages = total // 20
-    return render_template('main/search.html', title='Поиск', products=products, 
+    return render_template('main/search.html', title='Поиск', products=products, form=form,
                            next_url=next_url, prev_url=prev_url, page=page, pages=pages)
     
     
